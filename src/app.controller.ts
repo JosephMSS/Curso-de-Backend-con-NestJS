ee022accca907514c134a9b3864ab8503751201d
Joseph Morales Silva <josephzx1@hotmail.com>
2023-01-07 10:14:32 -0600

    GET: como recibir parametros

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 /**
@@ -23,5 +23,36 @@ export class AppController {
   @Get('nueva-ruta')
   getHelloV2(): string {
     return 'nueva ruta';
+  }
+  /**
+   ** Se recomienda que las rutas estén en plural
+   */
+  /**
+   * Podemos recibir el parámetro directamente con
+   * el decorador @Param y pasando como parámetro
+   * el nombre de la variable
+   * @param productId
+   * @returns
+   */
+  @Get('products/:productId')
+  getProduct(@Param('productId') productId: string): string {
+    return `Product ID:${productId}`;
+  }
+  /**
+   * O podemos recibir el el objeto
+   * params con todos los datos
+   * @param params
+   * @returns
+   */
+  @Get('categories/:categoryId')
+  getCategory(@Param() params: any): string {
+    return `Category ID:${params.categoryId}`;
+  }
+  @Get('people/:personId/category/:categoryId')
+  getCategoryByPerson(
+    @Param('categoryId') categoryId: string,
+    @Param('personId') personId: string,
+  ): string {
+    return `Category ID:${categoryId}  ${personId}`;
   }
 }
