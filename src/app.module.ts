2da807d96cfee745105b5c7e9eaffff3e4eb3c60
Joseph Morales Silva <josephzx1@hotmail.com>
2023-01-12 10:43:44 -0600

    Add user and orders controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OrdersController } from './controllers/orders/orders.controller';
+import { UsersController } from './controllers/users/users.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, OrdersController, UsersController],
   providers: [AppService],
 })
 export class AppModule {}
