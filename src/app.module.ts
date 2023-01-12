b0f6860250e0c74c90f478fbbdfcc34e5498112a
Joseph Morales Silva <josephzx1@hotmail.com>
2023-01-12 10:45:07 -0600

    Add controllers: customers, categories, products

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OrdersController } from './controllers/orders/orders.controller';
 import { UsersController } from './controllers/users/users.controller';
+import { CustomersController } from './controllers/customers/customers.controller';
+import { CategoriesController } from './controllers/categories/categories.controller';
+import { ProductsController } from './controllers/products/products.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, OrdersController, UsersController],
+  controllers: [AppController, OrdersController, UsersController, CustomersController, CategoriesController, ProductsController],
   providers: [AppService],
 })
 export class AppModule {}
